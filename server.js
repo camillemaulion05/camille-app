7540dad298fd5e6812887682b07a652d65dd412f
mcgmaulion <mcgmaulion05@gmail.com>
2021-03-08 16:01:54 +0800

    third commit

--- server.js
+++ server.js
@@ -1,12 +1,76 @@
 const cors = require("cors");
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
 
 global.__basedir = __dirname;
 
 var corsOptions = {
   origin: "http://localhost:8080"
 };
+
+var dbUrl = "mongodb+srv://camille:P@ssw0rd@vmo-ezloan.phft3.mongodb.net/vmo-ezloan?retryWrites=true&w=majority";
+/**
+ * Connect to MongoDB.
+ */
+const readLine = require('readline');
+
+const connect = () => {
+  setTimeout(() => mongoose.connect(dbUrl, {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  }), 1000);
+}
+
+mongoose.connection.on('connected', () => {
+  console.log('connected');
+});
+
+mongoose.connection.on('error', err => {
+  console.log('error: ' + err);
+  return connect();
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('disconnected');
+});
+
+if (process.platform === 'win32') {
+  const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.on('SIGINT', () => {
+    process.emit("SIGINT");
+  });
+}
+
+const gracefulShutdown = (msg, callback) => {
+  mongoose.connection.close(() => {
+    console.log(`Mongoose disconnected through ${msg}`);
+    callback();
+  });
+};
+
+process.once('SIGUSR2', () => {
+  gracefulShutdown('nodemon restart', () => {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+process.on('SIGINT', () => {
+  gracefulShutdown('app termination', () => {
+    process.exit(0);
+  });
+});
+process.on('SIGTERM', () => {
+  gracefulShutdown('Heroku app shutdown', () => {
+    process.exit(0);
+  });
+});
+
+connect();
 
 app.use(cors(corsOptions));
 
